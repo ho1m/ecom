7fb8c2b6f48f583e44eab8e82391014ad0588dce
ho1m <mohamed.yousef@utb.ecutbildning.se>
2021-11-09 14:00:25 +0100

    sql

---
+++ API/Data/createtables.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Addresses (
+	Id int not null identity primary key,
+	AddressLine nvarchar(150) not null,
+	ZipCode nvarchar(6) not null,
+	City nvarchar(100) not null
+)
+GO
+
+CREATE TABLE Users (
+	Id int not null identity primary key,
+	FirstName nvarchar(50) not null,
+	LastName nvarchar(50) not null,
+	Email nvarchar(100) not null unique,
+	Password nvarchar(100) not null,
+	AddressId int not null references Addresses(Id)
+)
+
+CREATE TABLE Products (
+	Id int not null identity primary key,
+	Title nvarchar(200) not null,
+	Description nvarchar(max) not null,
+	Price money not null,
+	Image nvarchar(max)
+)
